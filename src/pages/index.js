7f7b92e77b62af3065d9693253dd13f797c603fa
Max Kharandziuk <max.kharandziuk@gmail.com>
2020-04-20 13:26:10 +0200

    response form

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,26 @@ const IndexPage = () => (
     <h1>Hi Max</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
